f49ed32c3cda1ba8cad17d46a03b8062d35880f4
Soham Wankhede <sohamwankhede2022@gmail.com>
2026-04-07 13:42:06 +0530

    "Resolved Bugs that were in Review"

--- E-Commerce/src/main/java/com/mjs/ecommerce/controller/AuthController.java
+++ E-Commerce/src/main/java/com/mjs/ecommerce/controller/AuthController.java
@@ -144,6 +144,7 @@ public class AuthController {
     }
 
     private boolean isValidEmail(@Email String email) {
+
         return email != null &&
                 email.matches("^[A-Za-z0-9._%+\\-]+@[A-Za-z0-9.\\-]+\\.[A-Za-z]{2,}$");
     }
