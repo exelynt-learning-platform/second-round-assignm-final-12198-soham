b1e2e10541aa65e5750e33b747dcf48ae11ea3c4
Soham Wankhede <sohamwankhede2022@gmail.com>
2026-04-07 10:20:22 +0530

    "Resolved Bugs that were in Review"

--- E-Commerce/src/main/java/com/mjs/ecommerce/service/UserServiceImpl.java
+++ E-Commerce/src/main/java/com/mjs/ecommerce/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mjs.ecommerce.service;
 
 import com.mjs.ecommerce.constants.Constants;
+import com.mjs.ecommerce.exception.UserNotFoundException;
 import com.mjs.ecommerce.model.User;
 import com.mjs.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserById(Long id) {
-        return userRepository.findById(id).orElseThrow(()->new RuntimeException("User Not Found"));
+        return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(Constants.USER_NOT_FOUND));
     }
 
     @Override
